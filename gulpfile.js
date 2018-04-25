7a2bc0a5841d5b683b85cb58fb5c5e0b13fcc43c
Sean Denny <nektromc@gmail.com>
2018-04-25 18:29:10 -0400

    gulp add sitemap plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const gih = require('gulp-include-html');
+const sitemap = require('gulp-sitemap');
 
 gulp.task('html', function() {
     return gulp.src('./src/**/*.html')
@@ -16,10 +17,21 @@ gulp.task('netlify', function() {
     return gulp.src('./src/_headers').pipe(gulp.dest('bin'));
 });
 
+gulp.task('sitemap', function() {
+    return gulp.src(['./src/**/*.html','!./src/**/_*.html'], { read:false })
+    .pipe(sitemap({
+        siteUrl:'https://umassdacm.org',
+        getLoc: function(siteUrl, loc, entry) {
+            return loc.endsWith('.html') ? loc.substring(0, loc.length - 1 - 4) : loc;
+        }
+    }))
+    .pipe(gulp.dest('bin'));
+});
+
 gulp.task('site-dev', [
     'html', 'static'
 ]);
 
 gulp.task('default', [
-    'site-dev', 'netlify'
+    'site-dev', 'netlify', 'sitemap'
 ]);
